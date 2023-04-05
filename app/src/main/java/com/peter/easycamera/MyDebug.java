aab60bab1745b0ad9d7c9deac638078e2eccc58a
dopeter <nyliuzhen824@163.com>
2023-04-05 11:20:22 +0800

    modify package name

---
+++ app/src/main/java/com/peter/easycamera/MyDebug.java
@@ -0,0 +1,8 @@
+package com.peter.easycamera;
+
+/** Global constant to control logging, should always be set to false in
+ *  released versions.
+ */
+public class MyDebug {
+    public static final boolean LOG = false;
+}
